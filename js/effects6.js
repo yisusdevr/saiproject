59805b39381b056280aaeaf6e5d11dcf6809770e
YisusDev <yisus@gmail.com>
2023-09-04 01:18:36 -0600

    nueva pagina

--- js/effects6.js
+++ js/effects6.js
@@ -7,5 +7,29 @@ function typeWriter() {
     document.getElementById("title").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+    
   }
 }
+
+
+const formulario = document.getElementById('formularioContacto');
+const pepe = document.getElementById('pepe');
+
+formulario.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const nombreInput = document.getElementById('nombre');
+  const nombreValor = nombreInput.value;
+  const mensaje = document.getElementById("mensaje");
+  const mensajeValor = mensaje.value;
+  const correo = document.getElementById("correo");
+  const correoValor = correo.value;
+
+  Swal.fire({
+    icon: 'success',
+    title: '¡Formulario Enviado!',
+    text: `Gracias por tus comentarios ${nombreValor}. Tu mensaje ha sido enviado correctamente.`,
+  });
+
+  formulario.reset();
+});
