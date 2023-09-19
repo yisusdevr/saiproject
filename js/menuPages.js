f22a7127e3a1222049145ded8a47101eaf761cee
chaichor <christianpro313@gmail.com>
2023-09-19 00:09:31 -0600

    cambios al color de la pagina y logo

--- js/menuPages.js
+++ js/menuPages.js
@@ -2,7 +2,7 @@ function generarMenuPages(){
     const menuPages = `<nav class="navbar navbar-expand-lg  fixed-top"data-bs-theme="dark"  >
     <div class="container">
         <a class="navbar-brand" href="../index.html">
-            <img src="../img/sai_logo.png" alt="Logo de SAI" class="navbar-logo" width="100">
+            <img src="../img/logo_claro.png" alt="Logo de SAI" class="navbar-logo" width="100">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
